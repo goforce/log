c15f25c439e645c192561dafa462e4baeb0db84d
Olegs Denisovs <olegs@wingsforforce.com>
2014-10-22 11:25:11 +0300

    beginning

---
+++ logger.go
@@ -0,0 +1,40 @@
+package log
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+var Stderr io.Writer = os.Stderr
+var Discard io.Writer = ioutil.Discard
+
+var topics map[string]struct{} = make(map[string]struct{})
+
+func Println(topic string, v ...interface{}) bool {
+	if IsOn(topic) {
+		fmt.Println(v)
+		return true
+	}
+	return false
+}
+
+func SetOutput(w io.Writer) { log.SetOutput(w) }
+
+func On(topic string) {
+	for _, t := range strings.Split(topic, " ") {
+		topics[t] = struct{}{}
+	}
+}
+
+func Off(topic string) {
+	delete(topics, topic)
+}
+
+func IsOn(topic string) bool {
+	_, ok := topics[topic]
+	return ok
+}
